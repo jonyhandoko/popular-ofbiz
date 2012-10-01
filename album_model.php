31e4d94dcdf81f484ad91f336af727f95a2c1f01
jonyhandoko <asik01@gmail.com>
2012-10-01 17:16:40 +0700

    ini cuman test

--- album_model.php
+++ album_model.php
@@ -3,7 +3,7 @@
 if (!defined('BASEPATH'))
   exit('No direct script access allowed');
 
-/**
+/**test
  * Copyright (c) 2012, Aaron Benson - GalleryCMS - http://www.gallerycms.com
  * 
  * GalleryCMS is a free software application built on the CodeIgniter framework. 
